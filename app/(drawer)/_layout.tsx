a08b8bd8e21cc39397cf5e96c99ad530bd8a223d
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-02-15 22:27:28 +0200

    Install drizzle studio plugin and add Suspence

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,8 +1,13 @@
 import { Text, View } from "react-native";
 import { Drawer } from "expo-router/drawer";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
+import * as SQLite from "expo-sqlite";
+
+const db = SQLite.openDatabaseSync("reports.db");
 
 const Layout = () => {
+  useDrizzleStudio(db);
   return (
     <GestureHandlerRootView>
       <Drawer
