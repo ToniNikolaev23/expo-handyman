32d200fcddc94503d29bd37617081913c0843c0e
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-02-15 22:15:04 +0200

    Delete locations

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -1,8 +1,9 @@
 import LocationForm from "@/components/LocationForm";
+import LocationListItem from "@/components/LocationListItem";
 import { Location } from "@/types/interfaces";
 import { useSQLiteContext } from "expo-sqlite";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 const Page = () => {
   const db = useSQLiteContext();
@@ -26,6 +27,15 @@ const Page = () => {
   return (
     <View style={styles.container}>
       <LocationForm onSubmit={addLocation} />
+      <FlatList
+        data={locations}
+        renderItem={({ item }) => (
+          <LocationListItem location={item} onDelete={loadLocations} />
+        )}
+        ListEmptyComponent={
+          <Text style={styles.emptyText}>No locations added yet</Text>
+        }
+      />
     </View>
   );
 };
@@ -34,5 +44,11 @@ export default Page;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  emptyText: {
+    textAlign: "center",
+    marginTop: 16,
+    fontSize: 16,
+    color: "#666",
   },
 });
